fc096c12c5b2dd741fa004e781f320a4f4d0aeea
Omkar Ubale <oubale@gmail.com>
2023-07-23 21:02:50 -0400

    SETUP: Added config for Firebase Realtime Database

---
+++ app/src/main/java/edu/northeastern/coinnect/persistence/FirebaseDBHandler.java
@@ -0,0 +1,32 @@
+package edu.northeastern.coinnect.persistence;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseDBHandler {
+  private static final String TAG = "_FirebaseDBHandler";
+  private static FirebaseDatabase dbInstance;
+
+  private static FirebaseDBHandler INSTANCE;
+
+  private static void initDbReference() {
+    if (dbInstance == null) {
+      dbInstance = FirebaseDatabase.getInstance();
+    }
+  }
+
+  private FirebaseDBHandler() {
+    initDbReference();
+  }
+
+  public static FirebaseDBHandler getInstance() {
+    if(INSTANCE == null) {
+      INSTANCE = new FirebaseDBHandler();
+    }
+
+    return INSTANCE;
+  }
+
+  public FirebaseDatabase getDbInstance() {
+    return dbInstance;
+  }
+}
