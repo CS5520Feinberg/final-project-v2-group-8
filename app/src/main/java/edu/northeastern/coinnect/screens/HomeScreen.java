a5e0e3c433bf3364ee63cdd1f5302a020cde41fa
Dean Beebe <deanbeebe74@gmail.com>
2023-07-26 22:13:53 -0400

    FEATURE -- LoginActivity successfully implements Google 1-tap sign in.

---
+++ app/src/main/java/edu/northeastern/coinnect/screens/HomeScreen.java
@@ -0,0 +1,16 @@
+package edu.northeastern.coinnect.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.coinnect.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
