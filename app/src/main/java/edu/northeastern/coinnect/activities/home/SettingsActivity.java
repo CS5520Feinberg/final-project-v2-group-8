3f3cf4fe04ca59c825aa2a168e84e57ab6c92f79
PrajwalMathad <prajwalmathad966@gmail.com>
2023-08-04 00:38:38 -0400

    Added empty Activties for friends and configuration/settings.

---
+++ app/src/main/java/edu/northeastern/coinnect/activities/home/SettingsActivity.java
@@ -0,0 +1,37 @@
+package edu.northeastern.coinnect.activities.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import edu.northeastern.coinnect.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView2);
+        navView.setSelectedItemId(R.id.settings);
+
+        navView.setOnItemSelectedListener(item ->  {
+            if(item.getItemId() == R.id.friends) {
+                startActivity(new Intent(getApplicationContext(),FriendsActivity.class));
+                overridePendingTransition(0,0);
+
+            } else if(item.getItemId() == R.id.settings) {
+                return true;
+            } else {
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                overridePendingTransition(0,0);
+                return true;
+            }
+            return false;
+        });
+    }
+}
