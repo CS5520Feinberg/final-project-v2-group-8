f45eac6847550576201e9580d695613d6f9e9999
Omkar Ubale <oubale@gmail.com>
2023-08-05 15:56:15 -0400

    FEATURE: added groupTransactionId to groupTransactionModel class

--- app/src/main/java/edu/northeastern/coinnect/models/GroupTransactionModel.java
+++ app/src/main/java/edu/northeastern/coinnect/models/GroupTransactionModel.java
@@ -5,17 +5,23 @@ import java.util.List;
 import java.util.Map;
 
 public class GroupTransactionModel extends AbstractTransactionModel {
+  private int groupTransactionId;
 
   private List<GroupTransactionShareModel> shares;
 
-  public GroupTransactionModel(int id, String description, BigDecimal amount, int year, int month, int dayOfMonth, List<GroupTransactionShareModel> shares) {
+  public GroupTransactionModel(int id, String description, BigDecimal amount, int year, int month, int dayOfMonth, int groupTransactionId, List<GroupTransactionShareModel> shares) {
     super(id, description, amount, year, month, dayOfMonth);
+    this.groupTransactionId = groupTransactionId;
     this.shares = shares;
   }
 
   @Override
   public boolean getIsGroupTransaction() {
     return true;
+  }
+
+  public int getGroupTransactionId() {
+    return this.groupTransactionId;
   }
 
   public List<GroupTransactionShareModel> getGroupTransactionShares() {
